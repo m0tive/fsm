012683849a471165abd789a649538e0c671a795a
Peter Dodds <peter.dodds@naturalmotion.com>
2010-12-02 18:08:07 +0000

    Added some real tests for vector2 and tweeked the ignore file.

--- tests/cm2-Vector2.cpp
+++ tests/cm2-Vector2.cpp
@@ -1,5 +1,23 @@
 #include <gtest/gtest.h>
 
-TEST(Vector2, Empty) {
-    EXPECT_EQ( 1, 1 );
+#include <cm2/Vector2.hpp>
+
+class Vector2Test : public testing::Test {
+  protected:
+//    Vector2Test() : v1_(1,2), v_2(-0.5,-1) {}
+
+    cm2::Vector2 v0_;
+//    cm2::Vector2 v1_;
+//    cm2::Vector2 v2_;
+};
+
+TEST_F(Vector2Test, DefaultConstructor) {
+    ASSERT_DOUBLE_EQ( 0, v0_.x );
+    ASSERT_DOUBLE_EQ( 0, v0_.y );
+}
+
+TEST_F(Vector2Test, DataAccess) {
+    v0_.x = 1; v0_.y = 2;
+    ASSERT_DOUBLE_EQ( v0_.x, v0_.m_data[0] );
+    ASSERT_DOUBLE_EQ( v0_.y, v0_.m_data[1] );
 }
