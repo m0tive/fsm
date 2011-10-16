23974b7b2e6619d4579135307caeb7857982bbbf
Peter Dodds <peter@pddds.com>
2011-10-16 21:09:37 +0100

    Added missing dox for Matrix3 & Matrix4.

--- include/fsm/Matrix3.hpp
+++ include/fsm/Matrix3.hpp
@@ -35,6 +35,9 @@ public:
     /// <tt>| 0 0 1 |</tt>\n
     static const Matrix3 cIdentity;
 
+    //---------------------------------------
+    /// \brief Row major or column major flags.
+    /// This is used when converting to Matrix3 from Real[].
     enum Layout
     {
         kRowMajor = 0,
@@ -68,7 +71,7 @@ public:
     /// <tt>| 0 1 2 |</tt>\n
     /// <tt>| 3 4 5 |</tt>\n
     /// <tt>| 6 7 8 |</tt>\n
-    /// \param _mat[] - The matrix as an one-dimensional array of Reals.
+    /// \param _mat - The matrix as an one-dimensional array of Reals.
     /// \param _layout - specify input is column major layout: \par
     /// <tt>| 0 3 6 |</tt>\n
     /// <tt>| 1 4 7 |</tt>\n
@@ -83,11 +86,39 @@ public:
     /// \details Destructor
     ~Matrix3();
 
+    //---------------------------------------
+    /// \brief Value set.
+    /// Parameters are in the following format:
+    /// \par
+    /// <tt>| _m00 _m01 _m02 |</tt>\n
+    /// <tt>| _m10 _m11 _m12 |</tt>\n
+    /// <tt>| _m20 _m21 _m22 |</tt>\n
+    /// \param _m00 \param _m01 \param _m02
+    /// \param _m10 \param _m11 \param _m12
+    /// \param _m20 \param _m21 \param _m22
+    /// \param _layout - specify input is column major layout: \par
+    /// <tt>| _m00 _m10 _m20 |</tt>\n
+    /// <tt>| _m01 _m11 _m21 |</tt>\n
+    /// <tt>| _m02 _m12 _m22 |</tt>\n
+    /// \return A copy of the resulting matrix
     const Matrix3& set(
             Real _m00, Real _m01, Real _m02,
             Real _m10, Real _m11, Real _m12,
             Real _m20, Real _m21, Real _m22,
             Layout _layout = kRowMajor );
+    //---------------------------------------
+    /// \brief Value set.
+    /// \details Set constructor. Create a matrix from a one-dimensional array
+    /// of Reals. The default layout is: \par
+    /// <tt>| 0 1 2 |</tt>\n
+    /// <tt>| 3 4 5 |</tt>\n
+    /// <tt>| 6 7 8 |</tt>\n
+    /// \param _mat - The matrix as an one-dimensional array of Reals.
+    /// \param _layout - specify input is column major layout: \par
+    /// <tt>| 0 3 6 |</tt>\n
+    /// <tt>| 1 4 7 |</tt>\n
+    /// <tt>| 2 5 8 |</tt>\n
+    /// \return A copy of the resulting matrix
     const Matrix3& set( const Real _mat[], Layout _layout = kRowMajor );
 
     //---------------------------------------
@@ -97,8 +128,25 @@ public:
     /// \return A copy of the resulting matrix
     const Matrix3& operator =( const Matrix3& _rhs );
 
+    //---------------------------------------
+    /// \brief Matrix multiply.
+    /// Combine two matrices by multiplying lhs rows with rhs columns.
+    /// \param _rhs - the right hand side of the multiplication
+    /// \return The resulting matrix
     Matrix3 operator *( const Matrix3& _rhs ) const;
+    //---------------------------------------
+    /// \brief In-place pre-multiply.
+    /// Multiply this Matrix3 with another, using this as the rhs of the
+    ///   operation.
+    /// \param _lhs - the left hand side of the multiplication
+    /// \return A copy of the resulting matrix
     const Matrix3& preMultiply( const Matrix3& _lhs );
+    //---------------------------------------
+    /// \brief In-place post-multiply.
+    /// Multiply this Matrix3 with another, using this as the lhs of the
+    ///   operation.
+    /// \param _rhs - the right hand side of the multiplication
+    /// \return A copy of the resulting matrix
     const Matrix3& postMultiply( const Matrix3& _rhs );
 
     //---------------------------------------
@@ -116,16 +164,30 @@ public:
 
     //---------------------------------------
     /// \brief Get the transpose of this matrix.
-    /// \return A copy of this matrix is returned.
+    /// Create a copy of this matrix flipped along the major diagonal (top-left
+    ///   to bottom-right).
+    /// \return The resulting matrix
     Matrix3 transpose() const;
     //---------------------------------------
-    /// \brief Transpose a matrix _m.
+    /// \brief Transpose a matrix.
+    /// Flip a Matrix3 _m, in-place along the major diagonal (top-left to
+    ///   bottom-right).
     /// \param _m - the Matrix3 to be transposed.
     static void transpose( Matrix3& _m );
 
+    //---------------------------------------
+    /// \brief Calculate the determinate.
     Real determinate() const;
 
+    //---------------------------------------
+    /// \brief Calculate the inverse.
+    /// \return If determinate is not zero, the resulting inverse matrix,
+    ///   else Matrix3::kIdentity
     Matrix3 inverse() const;
+    //---------------------------------------
+    /// \brief Invert a matrix.
+    /// \param _m - the Matrix3 to invert in-place.
+    /// \return true if determinate is not zero and the inverse was possible.
     static bool invert( Matrix3& _m );
 
     Vector3 r0; ///< first row
--- include/fsm/Matrix4.hpp
+++ include/fsm/Matrix4.hpp
@@ -36,6 +36,9 @@ public:
     /// <tt>| 0 0 0 1 |</tt>\n
     static const Matrix4 cIdentity;
 
+    //---------------------------------------
+    /// \brief Row major or column major flags.
+    /// This is used when converting to Matrix4 from Real[].
     enum Layout
     {
         kRowMajor = 0,
@@ -75,7 +78,7 @@ public:
     /// <tt>|  4  5  6  7 |</tt>\n
     /// <tt>|  8  9 10 11 |</tt>\n
     /// <tt>| 12 13 14 15 |</tt>\n
-    /// \param _mat[] - The matrix as an one-dimensional array of Reals.
+    /// \param _mat - The matrix as an one-dimensional array of Reals.
     /// \param _layout - specify input is column major layout: \par
     /// <tt>|  0  4  8 12 |</tt>\n
     /// <tt>|  1  5  9 13 |</tt>\n
@@ -91,12 +94,45 @@ public:
     /// \details Destructor
     ~Matrix4();
 
+    //---------------------------------------
+    /// \brief Value set.
+    /// Parameters are in the following format:
+    /// \par
+    /// <tt>| _m00 _m01 _m02 _m03 |</tt>\n
+    /// <tt>| _m10 _m11 _m12 _m13 |</tt>\n
+    /// <tt>| _m20 _m21 _m22 _m23 |</tt>\n
+    /// <tt>| _m30 _m31 _m32 _m33 |</tt>\n
+    /// \param _m00 \param _m01 \param _m02 \param _m03
+    /// \param _m10 \param _m11 \param _m12 \param _m13
+    /// \param _m20 \param _m21 \param _m22 \param _m23
+    /// \param _m30 \param _m31 \param _m32 \param _m33
+    /// \param _layout - specify input is column major layout: \par
+    /// <tt>| _m00 _m10 _m20 _m30 |</tt>\n
+    /// <tt>| _m01 _m11 _m21 _m31 |</tt>\n
+    /// <tt>| _m02 _m12 _m22 _m32 |</tt>\n
+    /// <tt>| _m03 _m13 _m23 _m33 |</tt>\n
+    /// \return A copy of the resulting matrix
     const Matrix4& set(
             Real _m00, Real _m01, Real _m02, Real _m03,
             Real _m10, Real _m11, Real _m12, Real _m13,
             Real _m20, Real _m21, Real _m22, Real _m23,
             Real _m30, Real _m31, Real _m32, Real _m33,
             Layout _layout = kRowMajor );
+    //---------------------------------------
+    /// \brief Value set.
+    /// \details Set constructor. Create a matrix from a one-dimensional array
+    /// of Reals. The default layout is: \par
+    /// <tt>|  0  1  2  3 |</tt>\n
+    /// <tt>|  4  5  6  7 |</tt>\n
+    /// <tt>|  8  9 10 11 |</tt>\n
+    /// <tt>| 12 13 14 15 |</tt>\n
+    /// \param _mat - The matrix as an one-dimensional array of Reals.
+    /// \param _layout - specify input is column major layout: \par
+    /// <tt>|  0  4  8 12 |</tt>\n
+    /// <tt>|  1  5  9 13 |</tt>\n
+    /// <tt>|  2  6 10 14 |</tt>\n
+    /// <tt>|  3  7 11 15 |</tt>\n
+    /// \return A copy of the resulting matrix
     const Matrix4& set( const Real _mat[], Layout _layout = kRowMajor );
 
     //---------------------------------------
@@ -106,8 +142,25 @@ public:
     /// \return A copy of the resulting matrix
     const Matrix4& operator =( const Matrix4& _rhs );
 
+    //---------------------------------------
+    /// \brief Matrix multiply.
+    /// Combine two matrices by multiplying lhs rows with rhs columns.
+    /// \param _rhs - the right hand side of the multiplication
+    /// \return The resulting matrix
     Matrix4 operator *( const Matrix4& _rhs ) const;
+    //---------------------------------------
+    /// \brief In-place pre-multiply.
+    /// Multiply this Matrix4 with another, using this as the rhs of the
+    ///   operation.
+    /// \param _lhs - the left hand side of the multiplication
+    /// \return A copy of the resulting matrix
     const Matrix4& preMultiply( const Matrix4& _lhs );
+    //---------------------------------------
+    /// \brief In-place post-multiply.
+    /// Multiply this Matrix4 with another, using this as the lhs of the
+    ///   operation.
+    /// \param _rhs - the right hand side of the multiplication
+    /// \return A copy of the resulting matrix
     const Matrix4& postMultiply( const Matrix4& _rhs );
 
     //---------------------------------------
